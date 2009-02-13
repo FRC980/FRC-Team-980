23cfdee8a0b4ab9b3a3261f29d36c1119d995a74
Andrew J. Yeckel <ay2b@queue.org>
2009-02-13 23:59:04 +0000

    Tidy

--- 2009/frc980/trunk/workspace/WPILib/Joystick.cpp
+++ 2009/frc980/trunk/workspace/WPILib/Joystick.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                                                         */
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
 /*----------------------------------------------------------------------------*/
@@ -15,11 +15,11 @@ static bool joySticksInitialized = false;
 /**
  * Construct an instance of a joystick.
  * The joystick index is the usb port on the drivers station.
- * 
+ *
  * @param port The port on the driver station that the joystick is plugged into.
  */
-Joystick::Joystick(UINT32 port):m_ds(NULL), m_port(port), m_axes(NULL),
-m_buttons(NULL)
+Joystick::Joystick(UINT32 port)
+ : m_ds(NULL), m_port(port), m_axes(NULL), m_buttons(NULL)
 {
     InitJoystick(kNumAxisTypes, kNumButtonTypes);
 
@@ -35,17 +35,16 @@ m_buttons(NULL)
 
 /**
  * Version of the constructor to be called by sub-classes.
- * 
+ *
  * This constructor allows the subclass to configure the number of constants
  * for axes and buttons.
- * 
+ *
  * @param port The port on the driver station that the joystick is plugged into.
  * @param numAxisTypes The number of axis types in the enum.
  * @param numButtonTypes The number of button types in the enum.
  */
-Joystick::Joystick(UINT32 port, UINT32 numAxisTypes, UINT32 numButtonTypes):m_ds(NULL), m_port(port), m_axes(NULL),
-m_buttons
-(NULL)
+Joystick::Joystick(UINT32 port, UINT32 numAxisTypes, UINT32 numButtonTypes)
+  : m_ds(NULL), m_port(port), m_axes(NULL), m_buttons(NULL)
 {
     InitJoystick(numAxisTypes, numButtonTypes);
 }
@@ -129,7 +128,7 @@ float Joystick::GetThrottle()
 
 /**
  * Get the value of the axis.
- * 
+ *
  * @param axis The axis to read [1-6].
  * @return The value of the axis.
  */
@@ -140,10 +139,11 @@ float Joystick::GetRawAxis(UINT32 axis)
 
 /**
  * For the current joystick, return the axis determined by the argument.
- * 
- * This is for cases where the joystick axis is returned programatically, otherwise one of the
- * previous functions would be preferable (for example GetX()).
- * 
+ *
+ * This is for cases where the joystick axis is returned programatically,
+ * otherwise one of the previous functions would be preferable (for
+ * example GetX()).
+ *
  * @param axis The axis to read.
  * @return The value of the axis.
  */
@@ -169,10 +169,11 @@ float Joystick::GetAxis(AxisType axis)
 
 /**
  * Read the state of the trigger on the joystick.
- * 
+ *
  * Look up which button has been assigned to the trigger and read its state.
- * 
- * @param hand This parameter is ignored for the Joystick class and is only here to complete the GenericHID interface.
+ *
+ * @param hand This parameter is ignored for the Joystick class and is
+ * only here to complete the GenericHID interface.
  * @return The state of the trigger.
  */
 bool Joystick::GetTrigger(JoystickHand hand)
@@ -182,10 +183,11 @@ bool Joystick::GetTrigger(JoystickHand hand)
 
 /**
  * Read the state of the top button on the joystick.
- * 
+ *
  * Look up which button has been assigned to the top and read its state.
- * 
- * @param hand This parameter is ignored for the Joystick class and is only here to complete the GenericHID interface.
+ *
+ * @param hand This parameter is ignored for the Joystick class and is
+ * only here to complete the GenericHID interface.
  * @return The state of the top button.
  */
 bool Joystick::GetTop(JoystickHand hand)
@@ -206,10 +208,11 @@ bool Joystick::GetBumper(JoystickHand hand)
 
 /**
  * Get the button value for buttons 1 through 12.
- * 
- * The buttons are returned in a single 16 bit value with one bit representing the state
- * of each button. The appropriate button is returned as a boolean value. 
- * 
+ *
+ * The buttons are returned in a single 16 bit value with one bit
+ * representing the state of each button. The appropriate button is
+ * returned as a boolean value.
+ *
  * @param button The button number to be read.
  * @return The state of the button.
  **/
@@ -220,9 +223,9 @@ bool Joystick::GetRawButton(UINT32 button)
 
 /**
  * Get buttons based on an enumerated type.
- * 
+ *
  * The button type will be looked up in the list of buttons and then read.
- * 
+ *
  * @param button The type of button to read.
  * @return The state of the button.
  */
@@ -242,7 +245,7 @@ bool Joystick::GetButton(ButtonType button)
 
 /**
  * Get the channel currently associated with the specified axis.
- * 
+ *
  * @param axis The axis to look up the channel for.
  * @return The channel fr the axis.
  */
@@ -253,7 +256,7 @@ UINT32 Joystick::GetAxisChannel(AxisType axis)
 
 /**
  * Set the channel associated with a specified axis.
- * 
+ *
  * @param axis The axis to set the channel for.
  * @param channel The channel to set the axis to.
  */
