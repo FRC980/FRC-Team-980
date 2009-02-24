e46865161ab567f5ac72fe069989f5d55d474342
Andrew J. Yeckel <ay2b@queue.org>
2009-02-24 07:27:42 +0000

    feed the watchdog add some debug printf()s add ability to control belts via "Z" axis of JS 1

--- 2009/frc980/trunk/workspace/FRC980/operatorcontrol.cpp
+++ 2009/frc980/trunk/workspace/FRC980/operatorcontrol.cpp
@@ -25,12 +25,18 @@ void Main::OperatorControl()
 
     DriverStationLCD* pLCD = DriverStationLCD::GetInstance();
     Robot980* pRobot = Robot980::GetInstance();
+    Dashboard &d = DriverStation::GetInstance()->GetDashboardPacker();
+
+    printf("in Main::OperatorControl()\n");
+    pRobot->EnableTractionControl(false);
 
     while (IsOperatorControl() && !IsDisabled())
     {
-        if (jsDrive.GetRawButton(6))
+        GetWatchdog().Feed();
+
+        if (jsDrive.GetRawButton(6) || jsDrive.GetRawButton(11))
             pRobot->EnableTractionControl(true);
-        if (jsDrive.GetRawButton(7))
+        if (jsDrive.GetRawButton(7) || jsDrive.GetRawButton(10))
             pRobot->EnableTractionControl(false);
 
         float x = jsDrive.GetX();
@@ -40,13 +46,35 @@ void Main::OperatorControl()
         y = (y > 0) ? y * y : y * y * -1;
 
         pRobot->Drive(limit(y - x), limit(y + x), pLCD);
-        pRobot->RunBelts(fabs(jsBelts.GetY()), jsBelts.GetY());
+
+        float z = (1 - jsDrive.GetZ()) / 2;
+        if (jsDrive.GetRawButton(8) || jsDrive.GetRawButton(9))
+        {
+            if (jsDrive.GetRawButton(8))
+            {
+                pRobot->RunBelts(z, -z);
+                d.Printf("In  %f", z);
+            }
+            if (jsDrive.GetRawButton(9))
+            {
+                pRobot->RunBelts(z, z);
+                d.Printf("Out %f", z);
+            }
+        }
+        else
+        {
+            d.Printf("Z   %f", z);
+            pRobot->RunBelts(fabs(jsBelts.GetY()), jsBelts.GetY());
+        }
 
 
-        DashboardData::UpdateAndSend();
         pLCD->UpdateLCD();
+        DashboardData::UpdateAndSend();
+        Wait(0.05);
 
         while (!NextPeriodReady())
-            Wait(0.001);
+            Wait(0.01);
     }
+
+    printf("leaving Main::OperatorControl()\n");
 }
