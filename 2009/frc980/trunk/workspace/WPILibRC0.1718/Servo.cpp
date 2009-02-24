46d07f250bd333a00c8f87b8dcdec0c5b9624d2e
Andrew J. Yeckel <ay2b@queue.org>
2009-02-24 01:36:19 +0000

    tidy

--- 2009/frc980/trunk/workspace/WPILibRC0.1718/Servo.cpp
+++ 2009/frc980/trunk/workspace/WPILibRC0.1718/Servo.cpp
@@ -1,16 +1,17 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                                                         */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                            */
+/* Open Source Software - may be modified and shared by FRC teams. The code  */
+/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib. */
+/*---------------------------------------------------------------------------*/
 
 #include "Servo.h"
 
 /**
  * Common initialization code called by all constructors.
  *
- * InitServo() assigns defaults for the period multiplier for the servo PWM control signal, as
- * well as the minimum and maximum PWM values supported by the servo.
+ * InitServo() assigns defaults for the period multiplier for the servo
+ * PWM control signal, as well as the minimum and maximum PWM values
+ * supported by the servo.
  */
 void Servo::InitServo()
 {
@@ -21,9 +22,10 @@ void Servo::InitServo()
 /**
  * Constructor that assumes the default digital module.
  *
- * @param channel The PWM channel on the digital module to which the servo is attached.
+ * @param channel The PWM channel on the digital module to which the servo
+ * is attached.
  */
-Servo::Servo(UINT32 channel):PWM(channel)
+Servo::Servo(UINT32 channel) : PWM(channel)
 {
     InitServo();
 }
@@ -31,10 +33,12 @@ Servo::Servo(UINT32 channel):PWM(channel)
 /**
  * Constructor that specifies the digital module.
  *
- * @param slot The slot in the chassis that the digital module is plugged into.
- * @param channel The PWM channel on the digital module to which the servo is attached.
+ * @param slot The slot in the chassis that the digital module is plugged
+ * into.
+ * @param channel The PWM channel on the digital module to which the servo
+ * is attached.
  */
-Servo::Servo(UINT32 slot, UINT32 channel):PWM(slot, channel)
+Servo::Servo(UINT32 slot, UINT32 channel) : PWM(slot, channel)
 {
     InitServo();
 }
@@ -46,7 +50,8 @@ Servo::~Servo()
 /**
  * Set the servo position.
  *
- * Servo values range from 0.0 to 1.0 corresponding to the range of full left to full right.
+ * Servo values range from 0.0 to 1.0 corresponding to the range of full
+ * left to full right.
  *
  * @param value Position from 0.0 to 1.0.
  */
@@ -58,7 +63,8 @@ void Servo::Set(float value)
 /**
  * Get the servo position.
  *
- * Servo values range from 0.0 to 1.0 corresponding to the range of full left to full right.
+ * Servo values range from 0.0 to 1.0 corresponding to the range of full
+ * left to full right.
  *
  * @return Position from 0.0 to 1.0.
  */
@@ -70,11 +76,14 @@ float Servo::Get()
 /**
  * Set the servo angle.
  *
- * Assume that the servo angle is linear with respect to the PWM value (big assumption, need to test).
+ * Assume that the servo angle is linear with respect to the PWM value
+ * (big assumption, need to test).
  *
- * Servo angles that are out of the supported range of the servo simply "saturate" in that direction
- * In other words, if the servo has a range of (X degrees to Y degrees) than angles of less than X
- * result in an angle of X being set and angles of more than Y degrees result in an angle of Y being set.
+ * Servo angles that are out of the supported range of the servo simply
+ * "saturate" in that direction.  In other words, if the servo has a range
+ * of (X degrees to Y degrees) than angles of less than X result in an
+ * angle of X being set and angles of more than Y degrees result in an
+ * angle of Y being set.
  *
  * @param degrees The angle in degrees to set the servo.
  */
@@ -95,7 +104,8 @@ void Servo::SetAngle(float degrees)
 /**
  * Get the servo angle.
  *
- * Assume that the servo angle is linear with respect to the PWM value (big assumption, need to test).
+ * Assume that the servo angle is linear with respect to the PWM value
+ * (big assumption, need to test).
  * @return The angle in degrees to which the servo is set.
  */
 float Servo::GetAngle()
--- 2009/frc980/trunk/workspace/WPILibRC0.1718/Servo.h
+++ 2009/frc980/trunk/workspace/WPILibRC0.1718/Servo.h
@@ -1,8 +1,8 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                                                         */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                            */
+/* Open Source Software - may be modified and shared by FRC teams. The code  */
+/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib. */
+/*---------------------------------------------------------------------------*/
 
 #ifndef SERVO_H
 #define SERVO_H
@@ -12,16 +12,17 @@
 
 /**
  * Standard hobby style servo.
- * 
- * The range parameters default to the appropriate values for the Hitec HS-322HD servo provided
- * in the FIRST Kit of Parts in 2008.
+ *
+ * The range parameters default to the appropriate values for the Hitec
+ * HS-322HD servo provided in the FIRST Kit of Parts in 2008.
  */
-class Servo:public PWM, public SpeedController
+class Servo : public PWM, public SpeedController
 {
   public:
     explicit Servo(UINT32 channel);
     Servo(UINT32 slot, UINT32 channel);
     virtual ~ Servo();
+
     void Set(float value);
     float Get();
     void SetAngle(float angle);
