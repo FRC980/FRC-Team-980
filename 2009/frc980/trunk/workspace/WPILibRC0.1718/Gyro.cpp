a1854a12a6bfb1b8b3a0ff4c53814f39ec8f71b3
Andrew J. Yeckel <ay2b@queue.org>
2009-03-17 22:25:15 +0000

    tidy

--- 2009/frc980/trunk/workspace/WPILibRC0.1718/Gyro.cpp
+++ 2009/frc980/trunk/workspace/WPILibRC0.1718/Gyro.cpp
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                                                         */
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
 /*----------------------------------------------------------------------------*/
@@ -13,11 +13,13 @@
 
 /**
  * Initialize the gyro.
- * Calibrate the gyro by running for a number of samples and computing the center value for this
- * part. Then use the center value as the Accumulator center value for subsequent measurements.
- * It's important to make sure that the robot is not moving while the centering calculations are
- * in progress, this is typically done when the robot is first turned on while it's sitting at
- * rest before the competition starts.
+ * Calibrate the gyro by running for a number of samples and computing the
+ * center value for this part. Then use the center value as the
+ * Accumulator center value for subsequent measurements.  It's important
+ * to make sure that the robot is not moving while the centering
+ * calculations are in progress, this is typically done when the robot is
+ * first turned on while it's sitting at rest before the competition
+ * starts.
  */
 void Gyro::InitGyro()
 {
@@ -52,13 +54,13 @@ void Gyro::InitGyro()
     m_offset = ((float)value / (float)count) - (float)center;
 
     m_analog->SetAccumulatorCenter(center);
-    m_analog->SetAccumulatorDeadband(0);        ///< TODO: compute / parameterize this
+    m_analog->SetAccumulatorDeadband(0); ///< TODO: compute / parameterize this
     m_analog->ResetAccumulator();
 }
 
 /**
  * Gyro constructor given a slot and a channel.
- * 
+ *
  * @param slot The cRIO slot for the analog module the gyro is connected to.
  * @param channel The analog channel the gyro is connected to.
  */
@@ -71,9 +73,9 @@ Gyro::Gyro(UINT32 slot, UINT32 channel)
 
 /**
  * Gyro constructor with only a channel.
- * 
+ *
  * Use the default analog module slot.
- * 
+ *
  * @param channel The analog channel the gyro is connected to.
  */
 Gyro::Gyro(UINT32 channel)
@@ -112,8 +114,9 @@ Gyro::Gyro(AnalogChannel & channel)
 
 /**
  * Reset the gyro.
- * Resets the gyro to a heading of zero. This can be used if there is significant
- * drift in the gyro and it needs to be recalibrated after it has been running.
+ * Resets the gyro to a heading of zero. This can be used if there is
+ * significant drift in the gyro and it needs to be recalibrated after it
+ * has been running.
  */
 void Gyro::Reset()
 {
@@ -131,14 +134,15 @@ Gyro::~Gyro()
 
 /**
  * Return the actual angle in degrees that the robot is currently facing.
- * 
- * The angle is based on the current accumulator value corrected by the oversampling rate, the
- * gyro type and the A/D calibration values.
- * The angle is continuous, that is can go beyond 360 degrees. This make algorithms that wouldn't
- * want to see a discontinuity in the gyro output as it sweeps past 0 on the second time around.
- * 
- * @return the current heading of the robot in degrees. This heading is based on integration
- * of the returned rate from the gyro.
+ *
+ * The angle is based on the current accumulator value corrected by the
+ * oversampling rate, the gyro type and the A/D calibration values.  The
+ * angle is continuous, that is can go beyond 360 degrees. This make
+ * algorithms that wouldn't want to see a discontinuity in the gyro output
+ * as it sweeps past 0 on the second time around.
+ *
+ * @return the current heading of the robot in degrees. This heading is
+ * based on integration of the returned rate from the gyro.
  */
 float Gyro::GetAngle(void)
 {
@@ -159,10 +163,12 @@ float Gyro::GetAngle(void)
 
 /**
  * Set the gyro type based on the sensitivity.
- * This takes the number of volts/degree/second sensitivity of the gyro and uses it in subsequent
- * calculations to allow the code to work with multiple gyros.
- * 
- * @param voltsPerDegreePerSecond The type of gyro specified as the voltage that represents one degree/second.
+ * This takes the number of volts/degree/second sensitivity of the gyro
+ * and uses it in subsequent calculations to allow the code to work with
+ * multiple gyros.
+ *
+ * @param voltsPerDegreePerSecond The type of gyro specified as the
+ * voltage that represents one degree/second.
  */
 void Gyro::SetSensitivity(float voltsPerDegreePerSecond)
 {
@@ -171,7 +177,7 @@ void Gyro::SetSensitivity(float voltsPerDegreePerSecond)
 
 /**
  * Get the angle in degrees for the PIDSource base object.
- * 
+ *
  * @return The angle in degrees.
  */
 double Gyro::PIDGet()
--- 2009/frc980/trunk/workspace/WPILibRC0.1718/Gyro.h
+++ 2009/frc980/trunk/workspace/WPILibRC0.1718/Gyro.h
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                                                         */
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.  */
 /*----------------------------------------------------------------------------*/
@@ -14,14 +14,15 @@ class AnalogChannel;
 class AnalogModule;
 
 /**
- * Use a rate gyro to return the robots heading relative to a starting position.
- * The Gyro class tracks the robots heading based on the starting position. As the robot
- * rotates the new heading is computed by integrating the rate of rotation returned
- * by the sensor. When the class is instantiated, it does a short calibration routine
- * where it samples the gyro while at rest to determine the default offset. This is
+ * Use a rate gyro to return the robots heading relative to a starting
+ * position.  The Gyro class tracks the robots heading based on the
+ * starting position. As the robot rotates the new heading is computed by
+ * integrating the rate of rotation returned by the sensor. When the class
+ * is instantiated, it does a short calibration routine where it samples
+ * the gyro while at rest to determine the default offset. This is
  * subtracted from each sample to determine the heading.
  */
-class Gyro:public SensorBase, public PIDSource
+class Gyro : public SensorBase, public PIDSource
 {
   public:
     static const UINT32 kOversampleBits = 10;
@@ -30,11 +31,11 @@ class Gyro:public SensorBase, public PIDSource
     static const float kCalibrationSampleTime = 5.0;
     static const float kDefaultVoltsPerDegreePerSecond = 0.0125;
 
-         Gyro(UINT32 slot, UINT32 channel);
+    Gyro(UINT32 slot, UINT32 channel);
     explicit Gyro(UINT32 channel);
     explicit Gyro(AnalogChannel * channel);
     explicit Gyro(AnalogChannel & channel);
-         virtual ~ Gyro();
+    virtual ~ Gyro();
     float GetAngle();
     void SetSensitivity(float voltsPerDegreePerSecond);
     void Reset();
