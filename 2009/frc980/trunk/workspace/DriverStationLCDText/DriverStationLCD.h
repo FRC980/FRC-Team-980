e593107a6d9363d327d396f9cee71798ae7ad5a9
Andrew J. Yeckel <ay2b@queue.org>
2009-03-27 08:02:31 +0000

    tidy

--- 2009/frc980/trunk/workspace/DriverStationLCDText/DriverStationLCD.h
+++ 2009/frc980/trunk/workspace/DriverStationLCDText/DriverStationLCD.h
@@ -1,8 +1,8 @@
-/*--------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                           */
-/* Open Source Software - may be modified and shared by FRC teams. The code */
-/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib.*/
-/*--------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                            */
+/* Open Source Software - may be modified and shared by FRC teams. The code  */
+/* must be accompanied by the FIRST BSD license file in $(WIND_BASE)/WPILib. */
+/*---------------------------------------------------------------------------*/
 
 #ifndef __DRIVER_STATION_LCD_H__
 #define __DRIVER_STATION_LCD_H__
