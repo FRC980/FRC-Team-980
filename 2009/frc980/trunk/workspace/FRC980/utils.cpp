606ea7f57f13f37a6db988bb9b64c28373a43c88
Andrew J. Yeckel <ay2b@queue.org>
2009-04-16 18:40:14 +0000

    alliance collor char

--- 2009/frc980/trunk/workspace/FRC980/utils.cpp
+++ 2009/frc980/trunk/workspace/FRC980/utils.cpp
@@ -1,3 +1,5 @@
+#include <DriverStation.h>
+
 #include "utils.h"
 
 double limit(double val, double min /* = -1 */, double max /* = 1 */)
@@ -9,4 +11,15 @@ double limit(double val, double min /* = -1 */, double max /* = 1 */)
         return min;
 
     return val;
+}
+
+const char* const AllianceChar(enum DriverStation::Alliance a)
+{
+    if (DriverStation::kRed == a)
+        return "R";
+    if (DriverStation::kBlue == a)
+        return "B";
+    if (DriverStation::kInvalid == a)
+        return "I";
+    return "X";
 }
--- 2009/frc980/trunk/workspace/FRC980/utils.h
+++ 2009/frc980/trunk/workspace/FRC980/utils.h
@@ -1,7 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <DriverStation.h>
+
 double limit(double val, double min = -1, double max = 1);
+const char* const AllianceChar(enum DriverStation::Alliance a);
 
 #define ABS(x)	(((x) > 0) ? (x) : (-x))
 
