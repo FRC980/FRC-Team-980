9d0274d7e3452e2897af3e536a8f8ecff464ecb2
Chris Gilmer <chris.gilmer@gmail.com>
2010-03-19 16:08:49 +0000

    Returned the autonomous mode code to it's original form.  Moved the autonomous function that was there to the method Auton1().  We should now start developing more than one autonomous mode for play.

--- 2010/frc980/workspace/FRC980/main_autonomous.cpp
+++ 2010/frc980/workspace/FRC980/main_autonomous.cpp
@@ -36,14 +36,55 @@ void Main::AutonomousPeriodic(void)
 
     //--- Feed the watchdog
     GetWatchdog().Feed();
+    
+    //--- NOTE: THIS IS A STOPGAP UNTIL A WAY TO CHOOSE THIS MODE IS MADE
+    //    ALSO IT ALLOWS US TO START UTILIZING MORE THAN ONE AUTON MODE
+    iMode = 1;
 
+    //--- Switch to the correct autonomous mode
+    switch(iMode)
+    {
+	default:
+	
+	case 1:
+	    this->Auton1();
+	    break;
+	
+	case 2:
+	    this->Auton2();
+	    break;
+	
+	case 3:
+	    this->Auton3();
+	    break;
+	
+	case 4:
+	    this->Auton4();
+	    break;
+	
+	case 5:
+	    this->Auton5();
+	    break;
+	
+	case 6:
+	    this->Auton6();
+	    break;
+    }
+}
+
+//==============================================================================
+//==============================================================================
+void Main::Auton1()
+{
     float t = pTimerAuton->Get();
 
+    //--- In the first two seconds of the match drive forward
     if (t < 2)
     {
         pRobot->Drive(0.5, 0.5, 0.2); // left, right, roller
     }
 
+    //--- After two seconds stop the robot and fire
     if (t > 2)
     {
         pRobot->Drive(0, 0, 0); // stop
@@ -57,17 +98,11 @@ void Main::AutonomousPeriodic(void)
         }
     }
 
+    //--- After two and a half seconds rearm the kicker
     if (t > 2.5)
     {
         pRobot->ArmKicker();
     }
-}
-
-//==============================================================================
-//==============================================================================
-void Main::Auton1()
-{
-	
 }
 
 //==============================================================================
