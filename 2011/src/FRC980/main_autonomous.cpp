ea2212052725997f55fd4471ff9557de64d5fa80
Nikita Kitaev <nikitakit@gmail.com>
2011-02-20 18:48:04 -0800

    Working line tracking algorithm

--- 2011/src/FRC980/main_autonomous.cpp
+++ 2011/src/FRC980/main_autonomous.cpp
@@ -90,7 +90,7 @@ void AutonLineTrack(void)
     Robot980 *pRobot = Robot980::GetInstance();
     float time = pTimerAuton->Get();
     
-    static double forkProfile[] = {0.07, 0.07, 0.06, 0.06, 0.06, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    static double forkProfile[] = {0.5, 0.5, 0.4, 0.4, 0.3, 0.3, 0.03, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     static double straightProfile[] = {0.5, 0.5, 0.35, 0.35, 0.25, 0.25, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
     static double stopTime;
@@ -100,7 +100,7 @@ void AutonLineTrack(void)
     int timeInSeconds = (int) time;
     static int oldTimeInSeconds = -1;
 
-    double steeringGain = 0.65;
+    double steeringGain = 0.3;
     
     char binaryValue = pRobot->GetLineTracker(! goLeft);
     static char previousValue=0;
@@ -133,6 +133,7 @@ void AutonLineTrack(void)
     }
 
     speed = powerProfile[timeInSeconds];    // speed value for this time
+    speed = 0.15;
     turn = 0;                               // default to no turn
 
     switch (binaryValue) {
