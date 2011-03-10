b4c82c00579862f08ea34e2ae3bf42cad4c54e52
Nikita Kitaev <nikitakit@gmail.com>
2011-03-10 14:20:10 -0800

    x:[compiles] enable potentiometer status testing

--- 2011/src/FRC980/jsbuttons.h
+++ 2011/src/FRC980/jsbuttons.h
@@ -32,13 +32,13 @@
 //-- Aliases - drive controller
 #define DRIVE_SLOW_MODE         XB_BUTTON_BUMPER_LEFT
 #define DRIVE_FAST_MODE         XB_BUTTON_BUMPER_RIGHT
-#define DRIVE_LED_NONE          XB_BUTTON_A
-#define DRIVE_LED_TRIANGLE      XB_BUTTON_B
-#define DRIVE_LED_CIRCLE        XB_BUTTON_Y
-#define DRIVE_LED_SQUARE        XB_BUTTON_X
+#define DRIVE_LED_NONE          XB_BUTTON_A +20
+#define DRIVE_LED_TRIANGLE      XB_BUTTON_B +20
+#define DRIVE_LED_CIRCLE        XB_BUTTON_Y +20
+#define DRIVE_LED_SQUARE        XB_BUTTON_X +20
 
 //-- Aliases - drive controller for debugging
-#define DRIVE_PRINT_STATUS      XB_BUTTON_X +20
+#define DRIVE_PRINT_STATUS      XB_BUTTON_X
 #define DRIVE_PRINT_LINETRACKER XB_BUTTON_Y +20
 
 #define DRIVE_AUTON_INIT        XB_BUTTON_A +20
