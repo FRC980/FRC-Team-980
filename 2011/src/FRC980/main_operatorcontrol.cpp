bfafdbb12d4a9410c14a1ce0937c9884fc314ea2
Nikita Kitaev <nikitakit@gmail.com>
2011-03-11 10:19:18 -0800

    x:[compiles] .

--- 2011/src/FRC980/main_operatorcontrol.cpp
+++ 2011/src/FRC980/main_operatorcontrol.cpp
@@ -169,7 +169,7 @@ void Main::TeleopPeriodic(void)
         utils::message("Position #3: %D", target_position);
     }
 
-    int displacement = (int)(-pjsArm->GetY() * 120);
+    int displacement = (int)(-pjsArm->GetY() * 110);
     pRobot->SetPosition(target_position + displacement);
 
     if(pjsArm->GetRawAxis(XB_AXIS_TRIGGER) > 0.3)
