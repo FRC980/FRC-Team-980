df874c00f77809703990323278f1eca1ba4a337a
Nikita Kitaev <nikitakit@gmail.com>
2011-03-12 10:07:11 -0800

    x:[compiles] auton distance is 195 in

--- 2011/src/FRC980/main_autonomous.cpp
+++ 2011/src/FRC980/main_autonomous.cpp
@@ -291,7 +291,7 @@ void Auton6(void)
     float distance = pRobot->GetRightEncoder() - encoder_initial;
 
     float target_arm_height = 465;
-    float target_distance = 6.0*12.0; //195.0;
+    float target_distance = 195.0;
 
     static float initial_state_time = pTimerAuton->Get();
 
