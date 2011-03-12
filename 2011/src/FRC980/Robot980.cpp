02716904885e10e3ab60e59b1ae9698251bb1e01
Nikita Kitaev <nikitakit@gmail.com>
2011-03-11 17:41:07 -0800

    x:[compiles] assume wheel size 25

--- 2011/src/FRC980/Robot980.cpp
+++ 2011/src/FRC980/Robot980.cpp
@@ -269,7 +269,7 @@ void Robot980::LightLED(int num)
 //==========================================================================
 float Robot980::GetRightEncoder()
 {
-    return m_pscRight1->GetPosition() * 15.0;//3.14159 * WHEEL_DIAMETER;
+    return m_pscRight1->GetPosition() * 25.0;//3.14159 * WHEEL_DIAMETER;
 }
 
 //==========================================================================
