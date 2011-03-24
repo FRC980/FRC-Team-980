e86a3203c78199948e42f6528321f2d290258ec9
Nikita Kitaev <nikitakit@gmail.com>
2011-03-24 16:28:13 -0700

    x:[compiles] dont use claw timer

--- 2011/src/FRC980/main_operatorcontrol.cpp
+++ 2011/src/FRC980/main_operatorcontrol.cpp
@@ -227,9 +227,10 @@ void Main::TeleopPeriodic(void)
         open_pressed=false;
         close_pressed=false;
     }
-
-    if (pRobot->GetClawTimer() > 1.0/* && pRobot->GetClawCurrent() > 15.0*/)
+/*
+    if (pRobot->GetClawTimer() > 1.0 && pRobot->GetClawCurrent() > 15.0)
     {
         pRobot->RunClaw(0.0);
     }
+*/
 }
