d41792f639af7d69174dfcb0780e3d2fb04bc009
Nikita Kitaev <nikitakit@gmail.com>
2011-03-25 17:55:03 -0700

    x: Attempt auton mode with line tracking

--- 2011/src/FRC980/main_autonomous.cpp
+++ 2011/src/FRC980/main_autonomous.cpp
@@ -13,6 +13,7 @@ typedef enum
     AUTON_INIT,
     AUTON_CLOSE_CLAW,
     AUTON_RELEASE_CLAW,
+    AUTON_NUDGE_FORWARD,
     AUTON_DRIVE_FORWARD,
     AUTON_RAISE_ARM,
     AUTON_OPEN_CLAW,
@@ -25,7 +26,7 @@ auton_state_t auton_state = AUTON_INIT;
 
 static int iMode = 0;
 static Timer *pTimerAuton = new Timer;
-static bool goLeft = false;
+static bool bGoLeft = false;
 static bool bStraightLine = true;
 static bool bLineTrackModeInitialized = false;
 static float encoder_initial;
@@ -34,6 +35,7 @@ float GetSpeedStraight(void);
 float GetSpeedTurn(void);
 float GetSteeringGainStraight(void);
 float GetSteeringGainTurn(void);
+bool LineTrack(float t /*time*/, float stopTime, float speed, float gain, bool goLeft)
 void AutonLineTrack(void);
 void Auton1(void);
 void Auton2(void);
@@ -41,8 +43,6 @@ void Auton3(void);
 void Auton4(void);
 void Auton5(void);
 void Auton6(void);
-
-#define DEBUG_AUTON
 
 //==========================================================================
 void Main::AutonomousInit(void)
@@ -60,26 +60,30 @@ void Main::AutonomousInit(void)
     case 3:
     case 4:
     case 5:
-        goLeft = true;
+        bGoLeft = true;
         bStraightLine = true;
         bLineTrackModeInitialized = false;
         break;
 #else
     case 1:
-        goLeft = true;
+        bGoLeft = true;
         bStraightLine = false;
         bLineTrackModeInitialized = false;
         break;
     case 2:
-        goLeft = true;
+        bGoLeft = true;
         bStraightLine = true;
         bLineTrackModeInitialized = false;
         break;
     case 3:
-        goLeft = false;
+        bGoLeft = false;
         bStraightLine = false;
         bLineTrackModeInitialized = false;
         break;
+    case 4:
+        bGoLeft = false;
+        bStraightLine = false;
+        bLineTrackModeInitialized = false;
 #endif
 	case 6:
 	    pRobot->SetBrakes(true);
@@ -139,22 +143,6 @@ void Main::AutonomousPeriodic(void)
 
 float GetSpeedStraight(void)
 {
-/*
-    Robot980 *pRobot = Robot980::GetInstance();
-    float distance = pRobot->GetRightEncoder() - encoder_initial;
-
-    static const float initial_speed = 0.5;
-    static const float final_speed = 0.2;
-    static const float start_ramp_distance = 200.0; //50.0;
-    static const float final_distance = 200.0;
-
-    if (distance < start_ramp_distance)
-        return initial_speed;
-    else if (distance < final_distance)
-        return LINEAR_RAMP(distance,start_ramp_distance,final_distance,initial_speed,final_speed);
-    else
-        return 0.0;
-*/
     return 0.3;
 }
 
@@ -163,19 +151,7 @@ float GetSteeringGainStraight(void)
     Robot980 *pRobot = Robot980::GetInstance();
     float distance = pRobot->GetRightEncoder() - encoder_initial;
 
-    switch (iMode)
-    {
-    case 1:
-        return 0.15;
-    case 2:
-        return 0.15-distance*0.01/200.0;
-    case 3:
-        return 0.15-distance*0.03/200.0;
-    case 4:
-        return 0.15-distance*0.05/200.0;
-    case 5:
-        return 0.15-distance*0.10/200.0;
-    }
+    return 0.15-distance*0.12/200.0;
 }
 
 float GetSpeedTurn(void)
@@ -202,6 +178,80 @@ float GetSteeringGainTurn(void)
         return GetSpeedTurn() * LINEAR_RAMP(distance,100,131.4,1.0,1.5);
     else
         return 0.20;
+}
+
+bool LineTrack(float t /*time*/, float stopTime, float speed, float gain, bool goLeft)
+{
+    Robot980 *pRobot = Robot980::GetInstance();
+
+    static char previousValue = 0; // previous binary value
+
+    if (!bLineTrackModeInitialized)
+    {
+        // Print out a message displaying which
+        // line tracking mode we chose
+        utils::message("Line tracker auton mode");
+        utils::message("StraightLine: %d\n", bStraightLine);
+        utils::message("GoingLeft: %d\n", goLeft);
+
+        previousValue=0;
+
+        bLineTrackModeInitialized = true;
+    }
+
+    char binaryValue = pRobot->GetLineTracker(! goLeft);
+    double steeringGain = gain;
+    if (! goLeft) steeringGain *= -1.0;
+        // If going left, steer to the right, and vice versa
+    double turn = 0;
+
+
+    switch (binaryValue) {
+        // three bits in the format [outer, middle, inner]
+    case 1:
+        // just the inside sensor - drive straight
+        turn = 0;
+        break;
+    case 7:
+        // all sensors - maybe at the "T"
+        if (t > stopTime) {
+            pRobot->Drive(0.0,0.0);
+            return true;
+        }
+        break;
+    case 0:
+        // no sensors
+        if (previousValue == 0)
+        {
+            // If we start the match with no sensor reads!!!
+            utils::message("WARNING: no sensor data");
+            // apply the steering gain
+            turn = steeringGain;
+        }
+        if (previousValue == 1) {
+            // Previously it was just the outside sensor
+            // We drove away from the line, so steer back
+            turn = steeringGain;
+        }
+        else {
+            // In the rare case that the line is now on the
+            // opposite side of the sensors, steer back
+            turn = -steeringGain;
+        }
+        break;
+    default:
+        // If the line is read by the middle or outside sensors
+        turn = -steeringGain;
+    }
+    
+    utils::message("t=%2.2f sensor=%d speed=%1.2f turn=%1.2f distance=%f\n", t, binaryValue, speed, turn, pRobot->GetRightEncoder() - encoder_initial);
+    
+    // move the robot forward
+    pRobot->Drive(speed+turn, speed-turn);
+    
+    if (binaryValue != 0) previousValue = binaryValue;
+
+    return false;
 }
 
 void AutonLineTrack(void)
@@ -290,7 +340,7 @@ void AutonLineTrack(void)
     
     if (binaryValue != 0) previousValue = binaryValue;
 
-    Wait(0.01);
+    return false;
 }
 
 
@@ -320,6 +370,131 @@ void Auton4(void)
 {
     Robot980 *pRobot = Robot980::GetInstance();
     float t = pTimerAuton->Get();
+    float distance = pRobot->GetRightEncoder() - encoder_initial;
+
+    float target_arm_height = POT_CENTER_HIGH;
+    float target_distance = 195;
+
+    static float initial_state_time = pTimerAuton->Get();
+
+    switch (auton_state)
+    {
+    case AUTON_INIT:
+        auton_state = AUTON_CLOSE_CLAW;
+        //fall through
+    case AUTON_CLOSE_CLAW:
+        pRobot->CloseClaw();
+        if (t > 1.0)
+        {
+            pRobot->RunClaw(0.0);
+            auton_state = AUTON_RELEASE_CLAW;
+            initial_state_time = t;
+        }
+        break;
+    case AUTON_RELEASE_CLAW:
+        if ( (t - initial_state_time) < 0.2)
+        {
+            pRobot->Drive(-1.0,-1.0);
+        }
+        else if ( (t - initial_state_time) < 0.4)
+        {
+            pRobot->Drive(0.0,0.0);
+        }
+        else
+        {
+            auton_state = AUTON_NUDGE_FORWARD;
+            initial_state_time = t;
+        }
+        break;
+    case AUTON_NUDGE_FORWARD:
+        //return to where we started
+        if ( distance < 5 )
+        {
+            float speed = 0.20;
+            pRobot->Drive(speed,speed);
+            utils::message("Distance = %f\n", distance);
+        }
+        else
+        {
+            pRobot->Drive(0.0,0.0);
+            auton_state = AUTON_DRIVE_FORWARD;
+            initial_state_time = t;
+        }
+        break;
+    case AUTON_DRIVE_FORWARD:
+        pRobot->SetPosition(target_arm_height);
+        if (   (distance < target_distance) 
+            && (t  < 11.0 )                   )
+        {
+            float stopTime,speed,gain;
+            speed = bStraightLine ? GetSpeedStraight() : GetSpeedTurn();
+            gain = bStraightLine ? GetSteeringGainStraight() : GetSteeringGainTurn();
+            stopTime = bStraightLine ? 2.0 : 4.0;
+
+            bool atCross = LineTrack(t - initial_state_time, stopTime, speed, gain, bGoLeft);
+            if (atCross)
+            {
+                pRobot->Drive(0.0,0.0);
+                utils::message("Distance_final = %f (at cross)\n", distance);
+                auton_state = AUTON_RAISE_ARM;
+                initial_state_time = t;
+            }
+        }
+        else
+        {
+            pRobot->Drive(0.0,0.0);
+            utils::message("Distance_final = %f\n", distance);
+            auton_state = AUTON_RAISE_ARM;
+            initial_state_time = t;
+        }
+    case AUTON_RAISE_ARM:
+        if (
+            ((pRobot->GetPosition() - target_arm_height) < 10)
+            || ((pRobot->GetPosition() - target_arm_height) > -10)
+            )
+        {
+            pRobot->SetArmSpeed(0.0);
+            auton_state=AUTON_OPEN_CLAW;
+            initial_state_time = t;
+        }
+        else
+        {
+            utils::message("Arm at %f", pRobot->GetPosition());
+        }
+        break;
+    case AUTON_OPEN_CLAW:
+        if ( (t - initial_state_time) > 2.0)
+        {
+            auton_state = AUTON_LOWER_ARM;
+            initial_state_time = t;
+        }
+        else
+        {
+            pRobot->OpenClaw();
+        }
+        break;
+    case AUTON_LOWER_ARM:
+        //let gravity do the work
+        if ( (t - initial_state_time) > 2.0)
+        {
+            auton_state = AUTON_DRIVE_REVERSE;
+            initial_state_time = t;
+            utils::message("driving back");
+        }
+        break;
+    case AUTON_DRIVE_REVERSE:
+        if ( (t - initial_state_time) > 2.0)
+        {
+            pRobot->Drive(0.0, 0.0);
+            auton_state = AUTON_DONE;
+            initial_state_time = t;
+        }
+        else
+        {
+            pRobot->Drive(-0.25, -0.25);
+        }
+        break;
+    }
 }
 
 //==========================================================================
@@ -374,7 +549,7 @@ void Auton6(void)
     case AUTON_DRIVE_FORWARD:
         pRobot->SetPosition(target_arm_height);
         if (   (distance < target_distance) 
-            || (t  > 11.0 )                   )
+            && (t  < 11.0 )                   )
         {
             float speed = 0.40;
             pRobot->Drive(speed,speed);
