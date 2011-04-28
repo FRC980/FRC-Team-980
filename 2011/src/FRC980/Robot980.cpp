1c3d221ee139efb3da68b242377fe28633e4aacc
Nikita Kitaev <nikitakit@gmail.com>
2011-04-28 14:22:21 -0700

    Max arm speed when going down is 0.9

--- 2011/src/FRC980/Robot980.cpp
+++ 2011/src/FRC980/Robot980.cpp
@@ -100,7 +100,7 @@ Robot980::Robot980()
     //--- Set up PID loops
     m_pidArm->SetInputRange(POT_LOWER_LIMIT,POT_UPPER_LIMIT);
     m_pidArm->SetTolerance(POT_TOLERANCE /*%*/);
-    m_pidArm->SetOutputRange(-0.1,1.0);
+    m_pidArm->SetOutputRange(-0.09,1.0);
 
     //--- Define Drive Timer
     m_pTimerDrive->Reset();
