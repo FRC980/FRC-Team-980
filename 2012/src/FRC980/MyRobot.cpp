ced016a90c4cd93a50db314293a21d5b4e480362
Cameron Kaye <Shadow@ShadowsLaptop.(none)>
2012-01-26 20:55:04 -0800

    Base code for testing joystick

---
+++ 2012/src/FRC980/MyRobot.cpp
@@ -0,0 +1,65 @@
+#include "WPILib.h"
+#include "MyRobot.h"
+
+
+#define RUN_ONCE_VAR(joystick,button,var)              \
+    static bool var = false;                           \
+    if (! joystick->GetRawButton(button))              \
+    {                                                  \
+         var = false;                                  \
+    }                                                  \
+    else if (joystick->GetRawButton(button) &&         \
+             !var && (var=true))
+
+#define RUN_ONCE(joystick,button)                      \
+    RUN_ONCE_VAR(joystick,button,joystick##_##button##_pressed)
+
+MyRobot::MyRobot(void)
+{
+    jag1 = new Jaguar(Jaguar1);
+    joystick1 = new Joystick(1);
+    ds = DriverStation::GetInstance();
+    GetWatchdog().SetExpiration(0.1);
+}
+
+MyRobot::~MyRobot(void)
+{
+    delete jag1;
+    delete joystick1;
+    delete ds;
+}
+
+void MyRobot::Autonomous(void)
+{
+}
+
+void MyRobot::OperatorControl(void)
+{
+    bool printMessage = false;
+
+    GetWatchdog().SetEnabled(true);
+    GetWatchdog().Feed();
+
+    while(IsOperatorControl())
+    {
+        float x, y;
+        x = joystick1->GetX();
+        y = joystick1->GetY();
+        RUN_ONCE(joystick1, 1)
+        {
+            printf("\nTrigger pressed");
+            printf("\nx: %f", x);
+            printf("\ny: %f", y);
+        }
+
+        GetWatchdog().Feed();
+    }
+
+}
+
+void MyRobot::Drive(float speed)
+{
+    jag1->Set(speed);
+}
+
+START_ROBOT_CLASS(MyRobot);
---
+++ 2012/src/FRC980/MyRobot.h
@@ -0,0 +1,22 @@
+#include "WPILib.h"
+
+#ifndef _MyRobot_h_
+#define _MyRobot_h_
+
+#define Jaguar1     1
+
+class MyRobot : public SimpleRobot
+{
+    Jaguar *jag1;
+    Joystick *joystick1;
+    DriverStation *ds;
+
+public:
+    MyRobot(void);
+    ~MyRobot(void);
+    void Autonomous(void);
+    void OperatorControl(void);
+    void Drive(float); 
+};
+
+#endif
