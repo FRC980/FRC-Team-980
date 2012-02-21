05cb1d97c2dee2681ad3162fa5a4c834ca569dca
unknown <Daniel@Daniel-PC.(none)>
2012-02-20 18:06:57 -0800

    shooter added, ballpickup testing, doesnt work

--- 2012/src/FRC980/MyRobot.cpp
+++ 2012/src/FRC980/MyRobot.cpp
@@ -37,11 +37,16 @@ double limit(double val, double min = -1, double max = 1)
 }
 
 MyRobot::MyRobot(void)
-    : //m_pscShooter(new CANJaguar(20, CANJaguar::kSpeed))
-     m_pscLeft1(new CANJaguar(14))
+    : m_pscShooterMaster(new CANJaguar(15, CANJaguar::kSpeed))
+    , m_pscShooterSlave1(new CANJaguar(16, CANJaguar::kVoltage))
+    , m_pscShooterSlave2(new CANJaguar(17, CANJaguar::kVoltage))
+    , m_pscShooterSlave3(new CANJaguar(18, CANJaguar::kVoltage))
+    , m_pscLeft1(new CANJaguar(14))
     , m_pscLeft2(new CANJaguar(13))
     , m_pscRight1(new CANJaguar(12))
     , m_pscRight2(new CANJaguar(11))
+    , m_pscBallPickup(new Victor(1))
+    , m_pscBallFeeder(new Victor(2))
     , joystick1(new Joystick(1))
     , steeringwheel(new Joystick(2)) 
     , ds(DriverStation::GetInstance())
@@ -51,8 +56,6 @@ MyRobot::MyRobot(void)
     m_pscLeft1->ConfigMaxOutputVoltage(12.0);
     m_pscLeft1->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
 
-    m_pscLeft2->ConfigEncoderCodesPerRev(250);
-    m_pscLeft2->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
     m_pscLeft2->ConfigMaxOutputVoltage(12.0);
     m_pscLeft2->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
 
@@ -61,23 +64,22 @@ MyRobot::MyRobot(void)
     m_pscRight1->ConfigMaxOutputVoltage(12.0);
     m_pscRight1->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
  
-    m_pscRight2->ConfigEncoderCodesPerRev(250);
-    m_pscRight2->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
     m_pscRight2->ConfigMaxOutputVoltage(12.0);
     m_pscRight2->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);   
 
-    //m_pscShooter->ConfigEncoderCodesPerRev(250);
-    //m_pscShooter->SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
-    //m_pscShooter->ConfigMaxOutputVoltage(12);
+    m_pscShooterMaster->ConfigEncoderCodesPerRev(250);
+    m_pscShooterMaster->SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
+    m_pscShooterMaster->ConfigMaxOutputVoltage(12);
     
     float p, i, d;
     p = 0.001;
     i = 0.9;
     d = 0.0;
 
-    //m_pscShooter->SetPID(p,i,d);
-    //m_pscShooter->EnableControl();
+    m_pscShooterMaster->SetPID(p,i,d);
+    m_pscShooterMaster->EnableControl();
 
+    //increaded the SetExpiration(float) from 0.1 to 0.25 to attemt to fix watchdog not fed      error
     GetWatchdog().SetExpiration(0.1);
 }
 
@@ -87,7 +89,12 @@ MyRobot::~MyRobot(void)
     delete m_pscRight2;
     delete m_pscLeft1;
     delete m_pscLeft2;
-    //delete m_pscShooter;
+    delete m_pscShooterMaster;
+    delete m_pscShooterSlave1;
+    delete m_pscShooterSlave2;
+    delete m_pscShooterSlave3;
+    delete m_pscBallPickup;
+    delete m_pscBallFeeder;
     delete joystick1;
     delete steeringwheel;
     delete ds;
@@ -102,11 +109,14 @@ void MyRobot::OperatorControl(void)
     GetWatchdog().SetEnabled(true);
 
     while(IsOperatorControl())
-    {   //initialize gain and throttle varaibles
+    {   
+        GetWatchdog().Feed();
+
+    	//initialize gain and throttle varaibles
         float gain, throttle;
         gain = steeringwheel->GetX();
         throttle = joystick1->GetY();
-        
+
         gain = (gain > 0) ? gain * gain : gain * gain * -1;
         throttle = (throttle > 0) ? throttle * throttle * -1 : throttle * throttle;
 	//set default fLeft and fRight to throttle
@@ -114,47 +124,60 @@ void MyRobot::OperatorControl(void)
         float fRight = throttle;
 	
 //if statements for distributing power to left and right depending on gain value
-	message("%f\n", gain);
+	//message("%f\n", gain);
 	if(gain>0.05)
 	{
 	    fLeft = throttle+gain*2.0;
 	    fRight = throttle-gain*2.0;
 	    //print statment for fLeft,fRight
-	    message("%f,%f \n", fLeft, fRight);
+	   // message("%f,%f \n", fLeft, fRight);
 	}
 	else if(gain<-0.05)
 	{
 	    fLeft = throttle-gain*2.0;
 	    fRight = throttle+gain*2.0;
 	    //print staement for fLeft,fRight
-	    message("%f,%f \n", fLeft, fRight);
+	   // message("%f,%f \n", fLeft, fRight);
 	}
 
-        Drive(fLeft, fRight);
+       // Drive(fLeft, fRight);
+	message("pickup");
+        if(joystick1->GetRawButton(5))
+	{
+	   message("pickup");
+	   m_pscBallPickup->Set(0.5);
+	}
 
-        if(joystick1->GetRawButton(4))
-        {
-            printf("%f \n", GetRPM());
-        }
+	if(joystick1->GetRawButton(4))
+	{
+	   message("pickup");
+	   m_pscBallPickup->Set(-0.5);
+	}
 
-        GetWatchdog().Feed();
     }
 }
 
 float MyRobot::GetRPM(void)
 {
-    //return m_pscShooter->GetSpeed();
-    return NULL;
+    return m_pscShooterMaster->GetSpeed();
 }
 
 void MyRobot::Drive(float left, float right)
 {
-    //jag1->Set(speed*4500);
     m_pscLeft1->Set(limit(left));
     m_pscLeft2->Set(limit(left));
 
     m_pscRight1->Set(limit(-right));
     m_pscRight2->Set(limit(-right));
+}
+
+void MyRobot::SetShooterSpeed(float speed)
+{
+    m_pscShooterMaster->Set(speed);
+    float voltage = m_pscShooterMaster->GetOutputVoltage();
+    m_pscShooterSlave1->Set(voltage);
+    m_pscShooterSlave2->Set(voltage);
+    m_pscShooterSlave3->Set(voltage);
 }
 
 START_ROBOT_CLASS(MyRobot)
--- 2012/src/FRC980/MyRobot.h
+++ 2012/src/FRC980/MyRobot.h
@@ -8,12 +8,18 @@
 class MyRobot : public SimpleRobot
 {
 private:
-    CANJaguar *m_pscShooter;
+    CANJaguar *m_pscShooterMaster;
+    CANJaguar *m_pscShooterSlave1;
+    CANJaguar *m_pscShooterSlave2;
+    CANJaguar *m_pscShooterSlave3;
 
     CANJaguar *m_pscLeft1;
     CANJaguar *m_pscLeft2;
     CANJaguar *m_pscRight1;
     CANJaguar *m_pscRight2;
+
+    Victor *m_pscBallPickup;
+    Victor *m_pscBallFeeder;
 
     Joystick *joystick1;
     Joystick *steeringwheel;
@@ -27,6 +33,7 @@ public:
     void OperatorControl(void);
     float GetRPM(void);
     void Drive(float, float); 
+    void SetShooterSpeed(float);
 };
 
 #endif
