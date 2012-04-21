daece8bd2a465bb277fc9a9979085444e9a27122
unknown <Daniel@Daniel-PC.(none)>
2012-04-21 15:48:03 -0700

    all code not related to drive taken out

--- 2012/src/FRC980/MyRobot.h
+++ 2012/src/FRC980/MyRobot.h
@@ -12,44 +12,28 @@
 class MyRobot : public SimpleRobot
 {
 private:
-    CANJaguar *m_pscShooterMaster;
-    CANJaguar *m_pscShooterSlave1;
-
     CANJaguar *m_pscLeft1;
     CANJaguar *m_pscRight1;
 
-    Victor *m_pscBallPickup;
-    Victor *m_pscBallFeeder;
-    Victor *m_pscBridge;
-
     MyJoystick *joystick1;
-    MyJoystick *joystick2;
     MyJoystick *steeringwheel;
 
     DriverStation *ds;
-
-    //ADXL345_I2C *m_pAccelerometer;
-
-    Timer *m_bridge_timer;
 
 public:
     MyRobot(void);
     ~MyRobot(void);
     void Autonomous(void);
     void OperatorControl(void);
-    void RunBridge(bool);
-    void CheckStopBridge(void);
-    float GetRPM(void);
     void Drive(float, float);
     void DriveControlPosition(float, float);
     void DriveControlSpeed(float, float);
     void DriveControl(float, float);
-    void SetShooterSpeed(float);
     float GetRightEncoder(void);
     float GetLeftEncoder(void);
+    float GetSpeedRight(void);
+    float GetSpeedLeft(void);
     void SetBrakes(bool);
-    void PerformBalanceTrick(MyJoystick *joy);
-    void PerformBalanceTrickSpeed(MyJoystick *joy);
     void DriveControlMode(CANJaguar::ControlMode);
     int GetAutonMode(void);
 };
