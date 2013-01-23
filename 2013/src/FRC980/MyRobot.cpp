79694ce3b5e7b8a7cefe63fd134cb91f7b5c81af
Keiran Paster <user@ChrUbuntu.(none)>
2013-01-22 20:46:57 -0800

    basic drive control

--- 2013/src/FRC980/MyRobot.cpp
+++ 2013/src/FRC980/MyRobot.cpp
@@ -1,11 +1,81 @@
 #include "MyRobot.h"
+#include <math.h>
 
-MyRobot::MyRobot(void) {
+#define RUN_ONCE_VAR(joystick,button,var)              \
+    static bool var = false;                           \
+    if (! joystick->GetRawButton(button))              \
+    {                                                  \
+         var = false;                                  \
+    }                                                  \
+    else if (joystick->GetRawButton(button) &&         \
+             !var && (var=true))
 
+#define RUN_ONCE(joystick,button)                      \
+    RUN_ONCE_VAR(joystick,button,joystick##_##button##_pressed)
+
+typedef enum 
+{
+    AUTON_DRIVE,
+    AUTON_LOWER_ARM
+} auton_state_t;
+
+
+void message(char *fmt, ...)
+{
+    char message[256];
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(message, 256, fmt, args);
+    va_end(args);
+
+    setErrorData(message, strlen(message), 100);
+}
+
+double limit(double val, double min = -1, double max = 1)
+{
+    if (val > max)
+        return max;
+    if (val < min)
+        return min;
+
+    return val;
+}
+
+MyRobot::MyRobot(void) 
+    : m_pscLeft1(new CANJaguar(CAN_LEFT_CIM1)), 
+      m_pscLeft2(new CANJaguar(CAN_LEFT_CIM2)),
+      m_pscRight1(new CANJaguar(CAN_RIGHT_CIM1)),
+      m_pscRight2(new CANJaguar(CAN_RIGHT_CIM2))
+{
+    m_pscLeft1->ConfigEncoderCodesPerRev(US_DIGITAL_ENC_COUNTS);
+    m_pscLeft1->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+    m_pscLeft1->ConfigMaxOutputVoltage(MAX_JAGUAR_OUTPUT_VOLTAGE);
+    m_pscLeft1->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
+
+    m_pscLeft2->ConfigEncoderCodesPerRev(US_DIGITAL_ENC_COUNTS);
+    m_pscLeft2->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+    m_pscLeft2->ConfigMaxOutputVoltage(MAX_JAGUAR_OUTPUT_VOLTAGE);
+    m_pscLeft2->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
+
+    m_pscRight1->ConfigEncoderCodesPerRev(US_DIGITAL_ENC_COUNTS);
+    m_pscRight1->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+    m_pscRight1->ConfigMaxOutputVoltage(MAX_JAGUAR_OUTPUT_VOLTAGE);
+    m_pscRight1->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
+
+    m_pscRight2->ConfigEncoderCodesPerRev(US_DIGITAL_ENC_COUNTS);
+    m_pscRight2->SetPositionReference(CANJaguar::kPosRef_QuadEncoder);
+    m_pscRight2->ConfigMaxOutputVoltage(MAX_JAGUAR_OUTPUT_VOLTAGE);
+    m_pscRight2->ConfigNeutralMode(CANJaguar::kNeutralMode_Coast);
 }
 
 MyRobot::~MyRobot(void) {
-
+    delete m_pscLeft1;
+    delete m_pscLeft2;
+    delete m_pscRight1;
+    delete m_pscRight2;
+    delete joystick1;
+    delete steeringwheel;
 }
 
 void MyRobot::Autonomous(void) {
@@ -13,5 +83,50 @@ void MyRobot::Autonomous(void) {
 }
 
 void MyRobot::OperatorControl(void) {
+    GetWatchdog().SetEnabled(true);
+    while (IsOperatorControl() && IsEnabled()) {
+        GetWatchdog().Feed();
 
+        float gain, throttle;
+
+        gain = steeringwheel->GetX();
+        throttle = joystick1->GetY();
+
+	float eGain = (gain > 0) ? gain : gain * -1;
+	float eThrottle = (throttle > 0) ? throttle : throttle * -1;
+        //2.71828183
+	// eGain = pow(2.7, (2.4*eGain-3));
+	// eThrottle = pow(2.7, (3*eThrottle-3));
+	eGain = pow(2.71828183, (2.4*eGain-3));
+	eThrottle = pow(2.71828183, (3*eThrottle-3));
+
+        if(gain > -0.03 && gain < 0.03)
+            eGain = 0;
+        gain = (gain > 0) ? eGain : (eGain)* -1;
+        
+        if(throttle > -0.05 && throttle < 0.05)
+            eThrottle = 0;
+        throttle = (throttle > 0) ? (eThrottle)* -1 : (eThrottle);
+        //set default fLeft and fRight to throttle
+	float fLeft = throttle;
+        float fRight = fLeft;
+
+        //if statements for distributing power to left and right depending on gain value 
+    	if (gain>0.05 || gain<-0.05)
+        {	        
+            fLeft = throttle+(gain);
+	    fRight = throttle-(gain);
+        }
+
+        Drive(fLeft, fRight);
+
+        Wait(0.05);
+    }
+}
+
+void MyRobot::Drive(float right, float left) {
+    m_pscLeft1->Set(limit(left));
+    m_pscLeft2->Set(limit(left));
+    m_pscRight1->Set(limit(-right));
+    m_pscRight2->Set(limit(-right));
 }
--- 2013/src/FRC980/MyRobot.h
+++ 2013/src/FRC980/MyRobot.h
@@ -3,15 +3,38 @@
 #ifndef MyRobot_h_
 #define MyRobot_h_
 
+//==============================================================================
+// CAN Jaguar Outputs
+#define CAN_LEFT_CIM1               11  /*!< \def CAN_LEFT_CIM1 The CAN Jaguar device number for the Left CIM Motor */
+#define CAN_LEFT_CIM2               12  /*!< \def CAN_LEFT_CIM2 The CAN Jaguar device number for the Left CIM Motor */
+#define CAN_RIGHT_CIM1              13  /*!< \def CAN_RIGHT_CIM1 The CAN Jaguar device number for the Right CIM Motor */
+#define CAN_RIGHT_CIM2              14  /*!< \def CAN_RIGHT_CIM2 The CAN Jaguar device number for the Right CIM Motor */
+
+// Jaguar Outputs
+#define MAX_JAGUAR_OUTPUT_VOLTAGE   12.0        /*!< \def MAX_JAGUAR_OUTPUT_VOLTAGE The maximum output voltage of the CAN Jaguar */
+// Encoder setup information
+#define US_DIGITAL_ENC_COUNTS       4028 /*!< \def US_DIGITAL_ENC_COUNTS The number of encoder counts for the US Digital Encoders */
+
+//==============================================================================
+
 class MyRobot : public SimpleRobot
 {
 private:
-    
+    CANJaguar* m_pscLeft1;    
+    CANJaguar* m_pscLeft2;    
+
+    CANJaguar* m_pscRight1;    
+    CANJaguar* m_pscRight2;
+
+    Joystick *joystick1;
+    Joystick *steeringwheel;
+
 public:
     MyRobot(void);
     ~MyRobot(void);
     void Autonomous(void);
     void OperatorControl(void);
+    void Drive(float, float);
 };
 
 #endif
