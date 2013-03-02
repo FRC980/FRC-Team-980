7fe51f00bb429d2a945c2473af2737b504459bb1
Karpis <skarpisss@gmail.com>
2013-03-02 13:47:47 -0800

    jsbuttons

---
+++ 2013/src/FRC980/jsbuttons.h
@@ -0,0 +1,22 @@
+/////joystick3
+//need to arrange numbers later
+
+//Top Claw
+#define TOP_CLAW							1
+
+//Bottom Claw
+#define BOTTOM_CLAW							2	
+
+//Top Wheel 
+#define TOP_WHEEL							3
+
+//Bottom Wheel
+#define	BOTTOM_WHEEL							4
+
+//Release Climb Mechanism
+#define RELEASE_CLIMB_MECHANISM						5
+
+//Catapult Controls
+#define CATAPULT_WIND							6
+#define	CATAPULT_UNWIND							7
+#define CATAPULT_RELEASE						8
